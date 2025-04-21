37fd50b19d77e073da5ee56d83850aff81527e1d
yehjinjang <j95033176@gmail.com>
2025-04-21 21:41:55 +0900

    add: initial report page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SurveyForm from "./pages/SurveyForm";
+import Report from "./pages/Report"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<SurveyForm />} />
+        <Route path="/report" element={<Report />} /> 
       </Routes>
     </BrowserRouter>
   );
