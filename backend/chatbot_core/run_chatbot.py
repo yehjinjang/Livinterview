65006f14fa24ce9e97e69554a0cdc0951b977b8c
jinheesong <jinheesongg@gmail.com>
2025-04-26 15:14:28 +0900

    feat: add warning when total selected furniture items exceed 6

--- backend/chatbot_core/run_chatbot.py
+++ backend/chatbot_core/run_chatbot.py
@@ -1,7 +1,5 @@
-from chatbot_core.logic import run_conversation
+from backend.chatbot_core.logic import run_conversation
 
 if __name__ == "__main__":
     image_url = input("이미지 URL을 입력하세요: ").strip().strip('"')
     run_conversation(image_url)
-
-# "https://d1774jszgerdmk.cloudfront.net/1024/lm8eYHMOCkTo_TysRVv38"
