341b5abebf08ec2d7fa9d6e6537b97cf915e0727
sooin1516717 <sooin1516717@gmail.com>
2025-05-02 15:47:24 +0900

    fix: resolve route duplication in App.tsx and import ReportView

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SurveyForm from "./pages/SurveyForm";
-import Report from "./pages/Report"
-import RoomieHome from "./pages/RoomieHome"
-import ProfilePage from "./pages/ProfilePage"
-import RoomieChat from "./pages/RoomieChat"
-import RoomieResult from "./pages/RoomieResult"
-
+import Report from "./pages/Report";
+import ReportView from "./pages/ReportView";
+import RoomieHome from "./pages/RoomieHome";
+import ProfilePage from "./pages/ProfilePage";
+import RoomieChat from "./pages/RoomieChat";
+import RoomieResult from "./pages/RoomieResult";
 
 function App() {
   return (
@@ -18,9 +18,9 @@ function App() {
         <Route path="/survey" element={<SurveyForm />} />
         <Route path="/report" element={<Report />} />
         <Route path="/report/view" element={<ReportView />} />
-        <Route path="/profile" element={<ProfilePage />} /> 
-        <Route path="/roomie" element={<RoomieHome />} /> 
-        <Route path="/roomie/interior" element ={<RoomieChat />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/roomie" element={<RoomieHome />} />
+        <Route path="/roomie/interior" element={<RoomieChat />} />
         <Route path="/roomie/result" element={<RoomieResult />} />
       </Routes>
     </BrowserRouter>
