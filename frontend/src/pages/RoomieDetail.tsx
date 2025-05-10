789a1099ef793cc27165504ed8ce39789f90d180
jinheesong <jinheesongg@gmail.com>
2025-05-10 23:49:18 +0900

    chore: update example image URL in RoomDetail

--- frontend/src/pages/RoomieDetail.tsx
+++ frontend/src/pages/RoomieDetail.tsx
@@ -16,7 +16,7 @@ export default function RoomDetail({
   const [loading, setLoading] = useState(false);
 
   const defaultImageUrl =
-    "https://github.com/Livinterview/Livinterview/raw/dev/backend/empty-room-gen/inpaint/test.png";
+    "https://d1774jszgerdmk.cloudfront.net/512/36DlDeIYFl33V1mgDe-Cz";
   const imageUrl = room.imageUrl || defaultImageUrl;
 
   if (loading) {
